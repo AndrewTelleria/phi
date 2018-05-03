417885a77d5ec661a51810bc0df8986808696950
Andrew K Telleria <andrewktelleria@gmail.com>
2018-05-03 14:23:55 -0700

    StreamField - HomePage model added to the home app

--- home/models.py
+++ home/models.py
@@ -116,27 +116,127 @@ class StandardPage(Page):
 			related_name='+',
 			help_text='Landscape mode only; horizontal width between 1000px and 3000px'
 		)
-	body = StreamField(
-			BaseStreamBlock(), verbose_name="Page body", blank=True
-		)
+	# body = StreamField(
+	# 		BaseStreamBlock(), verbose_name="Page body", blank=True
+	# 	)
 	content_panels = Page.content_panels + [
 		FieldPanel('introduction', classname="full"),
-		StreamFieldPanel('body'),
+		# StreamFieldPanel('body'),
 		ImageChooserPanel('image'),
 	]
 
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    """
+	The Home Page. This looks slightly more complicated then it is. You can see
+	if you visit your site and edit the homepage that it is split between a:
+		- Hero area
+		- Body area
+		- A promotional area
+		- Moveable feature site sections
+    """
 
-    class Meta:
-    	verbose_name = "homepage"
+    # Hero section of HomePage
+    image = models.ForeignKey(
+    		'wagtailimages.Image',
+    		null=True,
+    		blank=True,
+    		on_delete=models.SET_NULL,
+    		related_name='+',
+    		help_text='Homepage image',
+    	)
+    hero_text = models.CharField(
+    		max_length=255,
+    		help_text='Write an introduction for the bakery',
+    	)
+    hero_cta = models.CharField(
+    		verbose_name='Hero CTA',
+    		max_length=255,
+    		help_text='Text to display on Call to Action',
+    	)
+    hero_cta_link = models.ForeignKey(
+    		'wagtailcore.Page',
+    		null=True,
+    		blank=True,
+    		on_delete=models.SET_NULL,
+    		related_name='+',
+    		verbose_name='Hero CTA link',
+    		help_text='Choose a page to link to for the Call to Action',
+    	)
+
+    # Body section of the HomePage
+    # body = StreamField(
+    # 		BaseStreamBlock(), verbose_name="Home content block", blank=True
+    # 	)
+
+    # Promo section of the HomePage
+    promo_image = models.ForeignKey(
+    		'wagtailimages.Image',
+    		null=True,
+    		blank=True,
+    		on_delete=models.SET_NULL,
+    		related_name='+',
+    		help_text='Promo image',
+    	)
+    promo_title = models.CharField(
+    		null=True,
+    		blank=True,
+    		max_length=255,
+    		help_text='Title to display above the promo copy',
+    	)
+    promo_text = RichTextField(
+    		null=True,
+    		blank=True,
+    		help_text='Write some promotional copy',
+    	)
+
+    # Featured sections on the HomePage
+    # You will see on templates/home/home_page.html that these are treated
+    # in different ways, and displayed in different areas of the page.
+    # Each list their children items that we access via the children function
+    # that we define on the individual Page models e.g. BlogIndexPage
+    featured_section_1_title = models.CharField(
+    		null=True,
+    		blank=True,
+    		max_length=255,
+    		help_text='Title to display above the promo copy',
+    	)
+    featured_section_1 = models.ForeignKey(
+    		'wagtailcore.Page',
+    		null=True,
+    		blank=True,
+    		on_delete=models.SET_NULL,
+    		related_name='+',
+    		help_text='First featured section for the homepage. Will display up to '
+    		'three child names.',
+    		verbose_name='Featured section 1',
+    	)
 
     content_panels = Page.content_panels + [
-    	FieldPanel('body', classname="full"),
-    	InlinePanel('gallery_images', label="Gallery images"),
+    	MultiFieldPanel([
+    		ImageChooserPanel('image'),
+    		FieldPanel('hero_text', classname="full"),
+    		MultiFieldPanel([
+    			FieldPanel('hero_cta'),
+    			PageChooserPanel('hero_cta_link'),
+    			]),
+    		], heading="Hero section"),
+    	MultiFieldPanel([
+    		ImageChooserPanel('promo_image'),
+    		FieldPanel('promo_title'),
+    		FieldPanel('promo_text'),
+    		], heading="Promo section"),
+    	# StreamFieldPanel('body'),
+		MultiFieldPanel([
+			FieldPanel('featured_section_1_title'),
+			PageChooserPanel('featured_section_1'),
+			], heading="Featured homepage section", classname="collapsible")
     ]
+
+    def __str__(self):
+    	return self.title
+
 
 
 class HomePageImageGallery(Orderable):
