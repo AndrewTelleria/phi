1e6a623920a47bf047b1d0259753d957f65b897c
Andrew K Telleria <andrewktelleria@gmail.com>
2018-05-03 09:32:04 -0700

    StreamField - StandardPage class in home app

--- home/models.py
+++ home/models.py
@@ -97,6 +97,34 @@ class FooterText(models.Model):
 		verbose_name_plural = "Footer Text"
 
 
+class StandardPage(Page):
+	"""
+	A generic content page. On this demo site we use it for an about page but
+	it could be used for any page content that only needs a title,
+	image, introduction, and body field
+	"""
+
+	introduction = models.TextField(
+			help_text='Text to describe the page',
+			blank=True,
+		)
+	image = models.ForeignKey(
+			'wagtailimages.Image',
+			null=True,
+			blank=True,
+			on_delete=models.SET_NULL,
+			related_name='+',
+			help_text='Landscape mode only; horizontal width between 1000px and 3000px'
+		)
+	body = StreamField(
+			BaseStreamBlock(), verbose_name="Page body", blank=True
+		)
+	content_panels = Page.content_panels + [
+		FieldPanel('introduction', classname="full"),
+		StreamFieldPanel('body'),
+		ImageChooserPanel('image'),
+	]
+
 
 
 class HomePage(Page):
